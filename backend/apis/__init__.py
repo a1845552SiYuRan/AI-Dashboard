69b341135891e318b1a6f498e294e8d318b87ba0
a1845552SiYuRan <a1845552@adelaide.edu.au>
2024-09-30 15:34:11 +0930

    Update __init__.py

--- backend/apis/__init__.py
+++ backend/apis/__init__.py
@@ -1,1 +1,14 @@
+from flask_restx import Api
 
+from .auth import api as auth
+from .ai import api as ai
+
+api = Api(
+    title='AI-Dashboard Backend',
+    version='1.0.0',
+    description='A RESTful API backend for the AI-Dashboard project.'
+    # All API metadatas
+)
+
+api.add_namespace(auth)
+api.add_namespace(ai)
